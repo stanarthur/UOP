ff0e333a60d9bd49ed19e41f3101279385e1965c
Your Name <stanhope.c.arthur@gmail.com>
2024-11-15 11:32:46 +0100

    feat: Added feature to find the most common character in TextAnalysisTool
    - Convert input text to lowercase for case-insensitive analysis
    - Calculate the frequency of each alphabetic character

--- 01_02/01/TextAnalysisTool.java
+++ 01_02/01/TextAnalysisTool.java
@@ -8,7 +8,7 @@ public class TextAnalysisTool {
         // User Input: Prompt the user to enter a paragraph or lengthy text
         System.out.println("Enter a paragraph or lengthy text:");
         String text = scanner.nextLine();
-        
+
         // Character Count: Calculate total characters in the input text
         int charCount = text.length();
         System.out.println("Total number of characters: " + charCount);
@@ -17,6 +17,16 @@ public class TextAnalysisTool {
         String[] words = text.split("\\s+");
         int wordCount = words.length;
         System.out.println("Total number of words: " + wordCount);
+
+         // Most Common Character: Find the most common character (case-insensitive)
+         String textLower = text.toLowerCase();
+         Map<Character, Integer> charFrequency = new HashMap<>();
+         
+         for (char c : textLower.toCharArray()) {
+             if (Character.isLetter(c)) {  // Only count alphabetic characters
+                 charFrequency.put(c, charFrequency.getOrDefault(c, 0) + 1);
+             }
+         }
 
         scanner.close();
         }
